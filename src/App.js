8cb43b826151da207cec5fe6f2e92b66d11fa874
Noam <noamru22@gmail.com>
2021-05-11 12:45:03 +0200

    scrolling logic almost done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,197 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import mp3 from "./assets/1.mp3";
+import React, { useState, useEffect } from "react";
+import { Howl, Howler } from "howler";
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [sound, setSound] = useState({});
+
+  useEffect(() => {
+    Howler.volume(1.0);
+  }, []);
+
+  const onClick = () => {
+    console.log("clicks");
+    setIsPlaying(true);
+    let sound = new Howl({
+      src: [mp3],
+      autoplay: false,
+      volume: 1,
+    });
+    setSound(sound);
+  };
+
+  const runOnScroll = () => {
+    console.log("scrolling");
+    setIsScrolling(true);
+  };
+
+  if (isPlaying) {
+    window.addEventListener("scroll", runOnScroll, { passive: true });
+
+    if (isScrolling) {
+      sound.play();
+    } else {
+      sound.pause();
+    }
+  }
+
+  function scrollStop(callback, refresh = 66) {
+    if (!callback || typeof callback !== "function") return;
+    let isScrolling;
+    window.addEventListener(
+      "scroll",
+      function (event) {
+        window.clearTimeout(isScrolling);
+        isScrolling = setTimeout(callback, refresh);
+      },
+      false
+    );
+  }
+
+  scrollStop(function () {
+    console.log("Scrolling has stopped.");
+    setIsScrolling(false);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={onClick}>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia odit
+        illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam
+        quo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        odit illum nostrum earum quidem sed nisi molestiae veritatis cupiditate,
+        asperiores eaque odio consequatur, accusamus neque. Dolorem, totam
+        porro! Mollitia, odio reprehenderit dolore, delectus dignissimos optio
+        perferendis harum obcaecati perspiciatis explicabo est quam consequatur
+        aspernatur unde laborum maiores sint saepe inventore commodi nisi neque
+        error facilis quidem nam! Officia, libero quos. Accusamus vero, dolorum
+        debitis numquam molestiae eligendi assumenda explicabo distinctio ipsum?
+        Eaque at minus veritatis esse, iure repellat amet voluptas praesentium
+        eveniet non soluta cumque vero beatae possimus, veniam perspiciatis a
+        unde autem doloribus. Recusandae maiores distinctio alias quisquam quo?
+      </div>
     </div>
   );
 }
